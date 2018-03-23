e18cb588491a027a7f02f183a687383d40d0534e
SmartDog <smartdog.chang@gmail.com>
2018-03-23 21:47:32 +0800

    #修改md

---
+++ AgriSystemCore/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgriSystemCore.Models;
+using AgriSystemCore_Service.Domain;
+using AgriSystemCore_Service.Service;
+using AgriSystemCore_Service.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace AgriSystemCore.Controllers
+{
+    [Authorize(Policy = "DashboardPolicy")]
+    public class DashboardController :  BaseController
+    {
+        public DashboardController(IHostingEnvironment env, IOptions<AgriOptions> ao) : base(env, ao)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
